b5c3d7d9b9bda298374a609af1238524ed8c6a85
Alexa <a.success@bk.ru>
2023-06-30 22:04:19 +0300

    lesson 12

--- js/script.js
+++ js/script.js
@@ -9,12 +9,29 @@ const personalMovieDB = {
 
 };
 
-const a = prompt ('Один из последних просмотренных фильмов?', ''),
-      b = prompt ('На сколько оцените его?', ''),  
-      c = prompt ('Один из последних просмотренных фильмов?', ''),
-      d = prompt ('На сколько оцените его?', '');  
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++){
+    const a = prompt ('Один из последних просмотренных фильмов?', ''),
+          b = prompt ('На сколько оцените его?', '');  
+
+    if(a != null && b != null && a != '' && b != '' && a.length <50 ){
+        personalMovieDB.movies[a] = b;
+        console.log ('done');
+    } else {
+        console.log('error');
+        i--;
+    }    
+}
+
+if (personalMovieDB.count < 10){
+    console.log('просмотренно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('вы класический зритель');
+} else if (personalMovieDB.count >= 30){
+    console.log('вы киноман');
+}else {
+    console.log('произошла ошибка');
+}
 
 console.log(personalMovieDB);
